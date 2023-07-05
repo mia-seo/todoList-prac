c88b8afd0aa38b30db816bcdfc239e87a7c4fb7e
Mia Seo <tjwlfif@gmail.com>
2023-07-05 14:20:01 +0900

    router 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import TodoList from "./pages/TodoList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignIn />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/todo",
+    element: <TodoList />,
+  },
+]);
 
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
